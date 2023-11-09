d07eeb70dcfd40c2f5f59de7aa0a65f92f20f4be
Claudemiro-Nogueira <claudemirobsp@gmail.com>
2023-11-09 15:40:29 -0300

    Adicao do projeto do redirecionador

---
+++ Redirecionador/script.js
@@ -0,0 +1,16 @@
+const botao = document.querySelector("#idsobre");
+const sobre1 = document.querySelector("sobre");
+
+
+
+
+
+
+
+
+botao.addEventListener("click", function(){
+    var sobre = document.getElementById("sobre1")
+    sobre.style.display = "flex";
+
+    
+});
